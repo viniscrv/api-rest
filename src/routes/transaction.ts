9d48bc8b418e1dba08e683dc7fca96bcc3c46b83
Vinícius <viniscrv@gmail.com>
2023-03-05 16:25:28 -0300

    feat: criado rota de resumo de transações

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -23,7 +23,17 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         return {
             transaction,
-        }
+        };
+    });
+
+    app.get("/summary", async () => {
+        const summary = await knex("transactions")
+            .sum("amount", { as: "amount" })
+            .first();
+
+        return {
+            summary,
+        };
     });
 
     app.post("/", async (request, reply) => {
