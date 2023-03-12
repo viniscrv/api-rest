fc9a1085c82420836a4e6ebe8eef28e5c6c2d85a
Vinícius <viniscrv@gmail.com>
2023-03-12 18:54:45 -0300

    test: adicionado teste de resumo

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -50,4 +50,53 @@ describe("transactions routes", () => {
             }),
         ]);
     });
+
+    test("user can get specific transaction", async () => {
+        const createTransactionResponse = await request(app.server)
+            .post("/transactions")
+            .send({ title: "New Transaction", amount: 5000, type: "credit" });
+
+        const cookies = createTransactionResponse.get("Set-Cookie");
+
+        const listTransactionsResponse = await request(app.server)
+            .get("/transactions")
+            .set("Cookie", cookies)
+            .expect(200);
+
+        const transactionId = listTransactionsResponse.body.transactions[0].id;
+
+        const getTransactionResponse = await request(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set("Cookie", cookies)
+            .expect(200);
+
+        expect(getTransactionResponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: "New Transaction",
+                amount: 5000,
+            })
+        );
+    });
+
+    test("user can get the summary", async () => {
+        const createTransactionResponse = await request(app.server)
+            .post("/transactions")
+            .send({ title: "New Transaction", amount: 5000, type: "credit" });
+
+        const cookies = createTransactionResponse.get("Set-Cookie");
+
+        await request(app.server)
+            .post("/transactions")
+            .set("Cookie", cookies)
+            .send({ title: "Debit transaction", amount: 2000, type: "debit" });
+
+        const summaryResponse = await request(app.server)
+            .get("/transactions/summary")
+            .set("Cookie", cookies)
+            .expect(200);
+
+        expect(summaryResponse.body.summary).toEqual({
+            amount: 3000,
+        });
+    });
 });
